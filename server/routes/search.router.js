e697b908cbbb323a344830ca28af07bfd889424f
Jake Knecht <Ljknecht91@gmail.com>
2019-07-25 11:43:27 -0500

    created search.router.js, .env, and GIPHY_API_KEY.

---
+++ server/routes/search.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const axios = require('axios');
+const router = express.Router();
+
+require('dotenv').config();
+
+const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+
+
+router.get('/', (req,res) => {
+    console.log('hitting search pull router');
+    
+    axios.get(`api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}`)
+    .then ((response) => {
+        console.log('back from giphy');
+        res.send(response.data)
+    })
+    .catch( (error) => {
+        console.log('error with getting gifs from giphy...', error);
+        resSendStatus(500);
+        
+    })
+});
+
+module.exports = router;
